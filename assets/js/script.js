2343c0472be9fd442a9789a4d17c84a5923ff410
shankar2311 <umabo2311@gmail.com>
2022-12-07 17:17:44 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            let playerChoice = this.getAttribute("data-choice");
+            alert(`you clicked ${playerChoice}`);
+        })
+    }
+})
+
+
 function runGmae(){
 
 }
@@ -15,5 +30,5 @@ function incrementPlayerScore() {
 }
 
 function incrementComputerScore() {
-    
+
 }
