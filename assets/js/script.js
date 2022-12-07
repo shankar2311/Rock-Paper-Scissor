6d44e7a71c834207f4d134ce7743b0fc7cd1c9d8
shankar2311 <umabo2311@gmail.com>
2022-12-07 17:33:20 +0000

    Add constant choices to DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
 // Wait for the DOM to finish loading before running the game
-// Get the button elements and add event listeners to them
-
 /**
  * Declare constants for possible choices
  */
 
 const choices = ["rock", "paper", "scissor"]
 
+// Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
         button.addEventListener("click", function(){
-            let playerChoice = this.getAttribute("data-choice");
+            let playerChoice = this.getAttribute("data-type");
             alert(`you clicked ${playerChoice}`);
         })
     }
